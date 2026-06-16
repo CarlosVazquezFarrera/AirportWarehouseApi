b08cab6055619b0901bb1b9a7420fdb5a1139c0d
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2026-06-16 13:56:17 -0600

    Add CORS policy for specific origins

--- AirportWarehouse/Program.cs
+++ AirportWarehouse/Program.cs
@@ -4,8 +4,29 @@ var builder = WebApplication.CreateBuilder(args);
 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy
+                          .WithOrigins(
+                              "http://localhost:4200",
+                              "http://localhost:80",
+                              "http://localhost",
+                              "http://137.184.230.117",
+                              "http://airportwarehouse.com.mx",
+                              "https://airportwarehouse.com.mx",
+                              "https://airportwarehouse.vercel.app"
+                           )
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                      });
+});
+
 builder.Services.AddHealthChecks();
-builder.Services.AddCorsConfig(MyAllowSpecificOrigins);
+//builder.Services.AddCorsConfig(MyAllowSpecificOrigins);
 builder.Services.AddControllerConfig();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddApplicationServices(builder.Configuration);
